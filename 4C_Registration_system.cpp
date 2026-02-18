ee4c26f25425245be67a53f8f937edf7f916521f
Ahnaf Farhan Ohi <ahnaffarhan1234@gmail.com>
2026-02-18 21:25:33 +0600

    4c

---
+++ 4C_Registration_system.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+
+    map<string,int>m;
+    while(n--)
+        {
+            string s;
+            cin>>s;
+            m[s]++;
+
+            if(m[s]==1)
+             cout<<"OK"<<endl;
+            else
+              cout<<s<<m[s]-1<<endl; 
+        }
+    
+    return 0;
+}
