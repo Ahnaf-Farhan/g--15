8c20be7a15da4e75e0d0c87e782100aabad96b3e
Ahnaf Farhan Ohi <ahnaffarhan1234@gmail.com>
2026-04-11 16:03:44 +0600

    688B

---
+++ 688B_Lovely_Palindromes.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string n;
+    cin>>n;
+
+    cout<<n;
+    for(int i=n.size()-1; i>=0; i--)
+        cout<<n[i];
+    return 0;
+}
